3b67ffd6bcdf9981d33e4e55af451aa90fca1ebf
juanlu46 <juanlu46@gmail.com>
2017-11-25 16:58:06 +0100

    asdfg

--- lector-huella.ino
+++ lector-huella.ino
@@ -1,6 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+//Dirección LCD
+#define LCD_ADDR 0x27
+#define LED_OFF 0
+#define LED_ON 1
+LiquidCrystal_I2C lcd(LCD_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+byte charRelleno[] = {
+  0x1F,
+  0x1F,
+  0x1F,
+  0x1F,
+  0x1F,
+  0x1F,
+  0x1F,
+  0x1F
+};
+
+byte charFlecha[] = {
+  0x10,
+  0x18,
+  0x1C,
+  0x1E,
+  0x1E,
+  0x1C,
+  0x18,
+  0x10
+};
+
+void setup() {
+
+  lcd.begin(16, 2);
+  lcd.createChar(0,charRelleno);
+  lcd.createChar(0,charFlecha);
+  lcd.setBacklight(LED_ON);
+  lcd.print("Cargando, espere");
 }
 
 void loop() {
